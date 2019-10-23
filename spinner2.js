e98d5b91538f25ef0114fb2834760e60311d3141
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-23 02:00:22 +0000

    Update spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,11 @@
-const characters = ['|   ', '/   ', '-   ', '\\   ', '|   ', '/   ', '-   ', '\\   '];
-timer = 1700;
-​
-characters.forEach(characeter => {
-  
-  setTimeout(() => process.stdout.write(`\r${characeter}`), timer)
-  timer -= 200;
-​
-})
+const characters = ['|   ', '/   ', '-   ', '\\   '];
+let timer = 0;
+for (let i = 0; i <= 20; i++){
+  setTimeout(() => process.stdout.write(`\r${characters[i % 4]}`), timer)
+  timer += 200;
+}
+// characters.forEach(characeter => {
+//   setTimeout(() => process.stdout.write(`\r${characeter}`), timer)
+//   timer -= 200;
+// });
+// setTimeout(() => {process.stdout.write("\n")}, );
